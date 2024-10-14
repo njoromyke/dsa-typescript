4c3aba81ddd663abc40e9ee8043b3c47fdde3fef
njoromyke <mykenjoroge01@gmail.co>
2024-10-14 11:58:41 +0300

    implement bubble sort algorithm

--- src/bubble-sort.ts
+++ src/bubble-sort.ts
@@ -1,19 +1,29 @@
-const arr = [4, 67, 23, 44, 22, 55, 2, 77, 3];
+/**
+ * outer loop runs n-1 times coz the values coz some part of the array will be already sorted on the first run
+ * inner loop will run n-i-1 times
+ * can be improved if the array is already sorted
+ * we can have a variable
+ */
+
+const arr = [7, 3, 9, 12, 11];
 
 function bubbleSort(arr: number[]): number[] {
   const n = arr.length;
-
-  // Outer loop: number of passes
+  let swapped = false;
+  // out loop to control how many times the inner loop runs
   for (let i = 0; i < n - 1; i++) {
-    // Inner loop: compare adjacent elements
+    // innner loop to compare and swap the numbers
     for (let j = 0; j < n - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
-        const temp = arr[j];
-        arr[j] = arr[j + 1];
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
 
-        arr[j + 1] = temp;
+        console.log(swapped)
+        swapped = true;
       }
     }
+    if (!swapped) break;
   }
 
   return arr;
